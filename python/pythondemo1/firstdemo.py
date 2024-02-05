90a2bc9a7e08e18d70aaf83b0bea24948f8192c1
Aruntamilai01 <aruntamilai01@gmail.com>
2024-02-05 12:17:30 +0530

    added

--- python/pythondemo1/firstdemo.py
+++ python/pythondemo1/firstdemo.py
@@ -4,9 +4,38 @@ b=20;
 print(a+b);
 print(a-b);
 print()"""
-a=input("enter the value a :");
+a=int(input("enter the value a :"));
 print (a);
-b=input("enter the value");
+b= int(input("enter the value :"));
 print (b);
-c=(a+b);
+c=int(input("enter the value c :"));
 print(c);
+print ("Enter the addition value",a+b-c);
+
+'''if a>b :
+    
+    print ("a greater than b ");
+else : 
+    print("b greater than a");
+print("a greater than b") if a>b else print("b greater than a")'''
+#print("a greater than b") if a>b else print (" a and b is equal") if a == b else ("b greater than a")
+
+'''if a>b and c>a :
+    print ("both condition same");
+if  a>b or c>a:
+    print ("any one condition is true ")'''
+'''if a < b:
+    if a < c and c > a:
+        print ("a less  than c");
+    elif a>b or c>a:
+        print ("any one condition is true ");
+    else :
+        print ("All not match");
+else :
+    print("not match");'''
+
+name=input("Enter your fname :");
+name1=input("enter your sname :");
+print (name);
+print (name1);
+print (name + " " + name1);
